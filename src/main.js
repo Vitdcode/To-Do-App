f6a12b245b1b5949ff2a2e99c684bf7a2b936b44
vitaly <vitalijdietrich@gmail.com>
2024-07-30 02:20:10 +0200

    styled the list items and put them further apart from each other

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import "./style.css";
 import { createListButton } from "./element-creation-functions.js";
 import { summonPrompt } from "./ui-functions.js";
 import { createListItem } from "./ui-functions.js";
-import { closePromptButton } from "./ui-functions.js";
-
 createListButton();
 summonPrompt();
-closePromptButton();
 createListItem();
